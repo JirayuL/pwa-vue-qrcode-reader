9966931807df8214aeb6b2de44ee91c91af7f2da
Jirayu Laungwilawan <jirayu.l@ku.th>
2019-01-21 20:25:13 +0700

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,6 @@
 <template>
   <div class="home">
     <p>Hello</p>
-    <!-- <ul>
-      <li v-for="(item, index) in linkQR" :key="index">
-        {{ item }}
-      </li>
-    </ul> -->
     <router-link to="/qr-scanner" exact><button>Scan</button></router-link>
   </div>
 </template>
